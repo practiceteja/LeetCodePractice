887c67c85bf6ba4c403a4c34435e5de8cb81be49
teja <83550117+practiceteja@users.noreply.github.com>
2024-05-17 22:49:25 +0530

    Time: 0 ms (100.00%), Space: 45.1 MB (73.59%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,17 +1,29 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int totalSum = 0;
-        for(int j = 0; j < nums.length; j++){
-            totalSum  += nums[j];
+        //int totalSum = 0;
+        //for(int j = 0; j < nums.length; j++){
+          //  totalSum  += nums[j];
+        //}
+        //int leftSum = 0, rightSum = totalSum  - nums[0];
+        //int i = 1;
+        //while(leftSum != rightSum && i < nums.length){
+         //   leftSum += nums[i-1];
+          //  rightSum -= nums[i];
+         //   i++;
+        //}
+        //int ans = (leftSum == rightSum) ? i-1 : -1;
+        //return ans;
+         int sum = 0;
+        int temp = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            sum += nums[i];
         }
-        int leftSum = 0, rightSum = totalSum  - nums[0];
-        int i = 1;
-        while(leftSum != rightSum && i < nums.length){
-            leftSum += nums[i-1];
-            rightSum -= nums[i];
-            i++;
+
+        for(int j=0 ; j < nums.length ; j++){
+            sum -= nums[j];
+            if(sum == temp)return j;
+            temp += nums[j];
         }
-        int ans = (leftSum == rightSum) ? i-1 : -1;
-        return ans;
+        return -1;
     }
 }
